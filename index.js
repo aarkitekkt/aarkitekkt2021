cc2d5cdfce9c21d64342b6d74087d8f0541310a8
aarkitekkt <johnson.brycej@gmail.com>
2020-12-21 07:54:57 -0700

    add basic section framework

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ console.log("aarkitekkt!");
 const navToggleBtn = document.getElementsByClassName("navToggle");
 const toggleElements = document.getElementsByClassName("toggleEl");
 
+
+// when nav element is clicked, toggle the navbar open or closed.
 function toggleNav() {
     for (let i = 0; i < toggleElements.length; i++) {
         toggleElements[i].classList.toggle("open");
