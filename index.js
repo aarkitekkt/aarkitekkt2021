d45963fcec2db35458686c567aab72b7ec5aa0df
aarkitekkt <johnson.brycej@gmail.com>
2020-12-22 07:50:04 -0700

    slide in nav logo when scroll past landing

--- index.js
+++ index.js
@@ -27,6 +27,35 @@ for (let i = 0; i < scrollLinks.length; i++) {
     })
 }
 
+// Slide in logo when scrolled past landing section.
 
+function debounce(func, wait = 15, immediate = true) {
+    var timeout;
+    return function () {
+        var context = this, args = arguments;
+        var later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
 
+const aarkitekktBtn = document.querySelector('#aarkitekktButton');
 
+function checkSlide() {
+    // slide nav logo in when scrolled past the landing section
+
+    const isPastLanding = window.scrollY > (window.innerHeight - 20);
+
+    if (isPastLanding) {
+        aarkitekktBtn.classList.add('isVisible');
+    } else {
+        aarkitekktBtn.classList.remove('isVisible');
+    }
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
